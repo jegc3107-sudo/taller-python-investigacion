d9193c0bbe4f4bee8341cef54adacc0a68ae97fc
jegc3107-sudo <jegc3107@gmail.com>
2026-05-17 09:09:18 -0500

    src/calculadora_simple.py

---
+++ src/calculadora_simple.py
@@ -0,0 +1,11 @@
+#Programa: Calculadora Basica
+#Autor: Juan Esteban Giraldo
+#Fecha 2026-05-15
+# Descripcion: Solicita dos numeros y permite realizar operaciones basicas
+
+#=====================
+#INGRESO DE DATOS
+#=====================
+num1 = float(input("Ingreasa Primer numero:  "))
+num2 = float(input("Ingrese segundo numero: "))
+
