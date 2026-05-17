51b61f273e113de69023e33b7d42a24c1c305174
jegc3107-sudo <jegc3107@gmail.com>
2026-05-17 11:04:27 -0500

    src/calculadora_simplr.py

--- src/calculadora_simple.py
+++ src/calculadora_simple.py
@@ -8,4 +8,28 @@
 #=====================
 num1 = float(input("Ingreasa Primer numero:  "))
 num2 = float(input("Ingrese segundo numero: "))
-
+print("\nMENÚ DE OPERACIONES")
+print("1. Sumar")
+print("2. Restar")
+print("3. Multiplicar")
+print("4. Dividir")
+opcion = input("Elija una opción (1-4): ")
+#=====================
+if opcion == '1':
+    resultado = num1 + num2
+    print(f"El resultado de la suma es: {resultado}")
+elif opcion == '2':
+    resultado = num1 - num2
+    print(f"El resultado de la resta es: {resultado}")
+elif opcion == '3':
+    resultado = num1 * num2
+    print(f"El resultado de la multiplicación es: {resultado}")
+elif opcion == '4':
+    if num2 != 0:
+        resultado = num1 / num2
+        print(f"El resultado de la división es: {resultado}")
+    else:
+        print("Error: No se puede dividir entre cero.")
+else:
+    print("Opción no válida.")
+    
